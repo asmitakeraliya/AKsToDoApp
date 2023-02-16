8ca8e8210ec3ccfa162a91a99cba8cc9963cd4e8
Asmita Keraliya <Asmita.Keraliya@gmail.com>
2023-02-16 14:25:25 +1100

    Changed api url in evironment.ts file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   prod: false,
-  apiUrl: 'http://localhost:8000',
+  apiUrl: 'https://localhost:7039',
   googleApiUrl:
     'https://translation.googleapis.com/language/translate/v2/?key=',
   googleApiKey: 'AIzaSyC2MLqIChbMU4ChQvXSGq_Aq6js-lsn4kc',
